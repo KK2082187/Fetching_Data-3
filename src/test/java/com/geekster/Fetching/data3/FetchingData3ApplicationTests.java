970194288e07094740b1bfb18bac40de32dc686b
Komal Kumar <kk2082187@gmail.com>
2023-04-12 16:08:17 +0530

    fetch data third

---
+++ src/test/java/com/geekster/Fetching/data3/FetchingData3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.Fetching.data3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingData3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
